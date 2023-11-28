d252d9b881bb29a733c5fcd2700f4a419f3db65d
EGGI <eggimugi03@gmail.com>
2023-11-28 10:11:37 +0700

    Menambahkan list di file index.php

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
     <h1>Hello World!</h1>
 
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero assumenda est minima sit, laborum mollitia eius voluptas illo, vel soluta facilis dignissimos ea porro? Similique quibusdam alias dignissimos molestias architecto?</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
